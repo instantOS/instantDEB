a490e5f3a52760c3b12d014cdf477bdfda706c7d
paperbenni <paperbenni@gmail.com>
2020-04-03 19:03:25 +0200

    add install script

--- build.sh
+++ build.sh
@@ -5,8 +5,22 @@ if [ -e "PKGBUILD" ]; then
     exit 1
 fi
 
-rm -rf .debcache
+[ -e .debcache ] && rm -rf .debcache
+
 mkdir .debcache
 cp PKGBUILD debcache/
 cd .debcache
 /usr/share/instantdeb/controlgen.sh
+mkdir -p package/DEBIAN
+mv control package/DEBIAN/
+export pkgdir="$(realpath ./package)"
+source PKGBUILD
+
+if ! echo $source | grep -q 'git\+'; then
+    echo "no git source found"
+    exit
+fi
+
+prepare
+build
+package
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# installs ideb
+# doesnt use package management because its a special snowflake
+
+echo "installing ideb"
+
+# local install if run in directory,
+# fetch from github if ran from other sources
+if ! [ -e ideb.sh ]; then
+    mkdir -p ~/.cache/ideb
+    cd ~/.cache/ideb
+    git clone --depth=1 https://github.com/instantos/instantDEB
+    cd instantDEB
+fi
+
+chmod +x ./*.sh
+
+sudo cp ideb.sh /usr/bin/ideb
+sudo chmod 755 /usr/bin/ideb
+
+[ -e /usr/share/ideb ] || sudo mkdir -p /usr/share/ideb
+
+sudo cp *.sh /usr/share/ideb
+sudo chmod 755 /usr/share/ideb/*
