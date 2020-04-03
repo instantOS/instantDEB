0ca49846bb6d715aeaf2144935f326fb9e01fe5e
paperbenni <paperbenni@gmail.com>
2020-04-03 18:26:21 +0200

    add controlgen

---
+++ controlgen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# generate a debian control file from a PKGBUILD
+
+if ! [ -e PKGBUILD ]; then
+    echo "PKGBUILD not found"
+    exit 1
+fi
+
+source PKGBUILD/
+
+echo "Package: $pkgname" >control
+if type pkgver; then
+    pkgver="$(pkgver)"
+fi
+
+echo "Version: $pkgver" >>control
+echo "Architecture: ${arch:-any}" >>control
+echo "Maintainer: instantDEB <paperbenni@gmail.com>" >>control
+echo "Depends: bash" >>control
+echo "Recommends: bash" >>control
+echo "Section: misc" >>control
+echo "Priority: optional" >>control
+
+if [ -n "$pkgdesc" ]; then
+    echo "Description: $pkgdesc" >>control
+else
+    echo "Description: auto generated package by instantDEB" >>control
+fi
+
+echo "Original-Maintainer: paperbenni <paper.benni@gmail.com>" >>control
