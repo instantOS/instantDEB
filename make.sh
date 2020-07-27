cb3195cbedfeaea0027fa7a077c60f75c7e26dd1
Robin <dequanzh@usc.edu>
2020-07-27 09:45:54 -0700

    download all necessary files from upstream

---
+++ make.sh
@@ -0,0 +1,13 @@
+download() {
+   for i in $(curl https://instantos.surge.sh/ | link | grep -Eo '"(.*xz)"' | tr -d '"' )
+   do
+       wget "https://instantos.surge.sh/$i"
+   done
+}
+
+unpack() {
+    for f in $(find . -name '*.xz')
+    do
+        tar xvf "$f" | tee file_list.txt
+    done
+}
