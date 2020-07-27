7ed93c25fefd12d985c9ceadef6db448ebd1b406
Robin <dequanzh@usc.edu>
2020-07-27 11:14:33 -0700

    update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 download() {
-   for i in $(curl https://instantos.surge.sh/ | link | grep -Eo '"(.*xz)"' | tr -d '"' )
+   for i in $(curl https://instantos.surge.sh/ | link | grep -Eo '"(.*xz)"' | tr -d '"')
    do
        wget "https://instantos.surge.sh/$i"
    done
@@ -11,3 +11,5 @@ unpack() {
         tar xvf "$f" | tee file_list.txt
     done
 }
+
+"$@"
