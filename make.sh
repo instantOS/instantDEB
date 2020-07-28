93ecc7dbef6529222a2c0db0f730b862203bd0b6
your name <you@example.com>
2020-07-28 22:56:37 +0000

    add cache check

--- make.sh
+++ make.sh
@@ -5,7 +5,9 @@ get-link() {
 download() {
    for i in $(curl https://instantos.surge.sh/ | get-link | grep -Eo '"(.*xz)"' | tr -d '"')
    do
-       wget "https://instantos.surge.sh/$i"
+       if [ ! -f "$i" ] ; then
+	   wget "https://instantos.surge.sh/$i"
+       fi
    done
 }
 
